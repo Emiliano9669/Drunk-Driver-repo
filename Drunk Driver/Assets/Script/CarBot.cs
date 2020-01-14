2153ddfed019917870b0c041fa4e06c162b20618
Emiliano <emiliano9669@gmail.com>
2020-01-14 19:38:52 -0300

    mecanicas del auto terminadas

---
+++ Drunk Driver/Assets/Script/CarBot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBot : MonoBehaviour
+{
+    public float speed;
+    public Vector3 direction;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(direction * Time.deltaTime * speed);
+    }
+
+}
---
+++ Drunk Driver/Assets/Script/Player.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float accelerationPower;
+    public float steeringPower;
+    private float steeringAmount, speed, direction;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        steeringAmount = 0;
+    }
+
+
+
+    private void FixedUpdate()
+    {
+        steeringAmount = TouchSteering(); // - Input.GetAxis("Horizontal")
+        speed = 1  * accelerationPower; //esto tendria que ser siempre 1  # Input.GetAxis("Vertical") #
+        direction = Mathf.Sign(Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up))); //esto tendria que ser siempre 1
+
+        if (rb.rotation <= 20 && rb.rotation >= -20)
+        {
+            rb.rotation += steeringAmount * steeringPower * rb.velocity.magnitude * direction;  // aca podemos poner el limite determinado para doblar (ejemplo: de -20 a 20)
+        }
+        else
+        {
+            if (rb.rotation >= 20)
+                rb.rotation = 20;
+            else
+                rb.rotation = -20;
+        }
+
+
+        rb.AddRelativeForce(Vector2.up * speed);
+        rb.AddRelativeForce(-Vector2.right * rb.velocity.magnitude * steeringAmount / 2);
+    }
+
+
+    private float TouchSteering()
+    {
+        if (Input.touchCount > 0)
+        {
+            steeringAmount += 0.1f;
+        }
+        else
+        {
+            steeringAmount -= 0.1f;
+        }
+
+        if (steeringAmount > 1)
+            steeringAmount = 1;
+        else if (steeringAmount < -1)
+            steeringAmount = -1;
+        return steeringAmount;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        string nameObject = collision.transform.tag;
+        if (nameObject == "CarBot"  || nameObject == "Border")
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+
+    }
+
+ 
+
+
+}
